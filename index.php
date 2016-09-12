dc66b85d9a0a9d92416dc5320a7dfe501ca24238
Michael Rodby <github@rodby.org>
2016-09-12 04:34:04 +0000

    Change index.html to list channels in left pane and show usage in right pane.

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+ini_set('html_errors', false);
+
+$servername = "localhost";
+$username = $_SERVER["DB_USER"];
+$password = $_SERVER["DB_PASSWORD"];
+$dbname = "eMonitor";
+
+// Create database connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error)
+    die("Database connection failed: " . $conn->connect_error);
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -6,52 +21,125 @@
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Greener Circuits</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
+    <style>
+
+      div.content {
+        margin-left: 150px;
+      }
+      div.side {
+        position: fixed;
+        height: 100%;
+        top: 0;
+        left: 0;
+        width: 180px;
+        padding: 10px;
+      }
+      ul {
+        margin: 0;
+        padding: 0;
+      }
+      ul li {
+        color: #fff;
+      }
+    </style>
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+    <script type="text/javascript">
+
+      // Load the Visualization API and the corechart package.
+      google.load("visualization", "1", {packages:["corechart"]});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // The callback itself
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Time', 'Watts'],
+          <?php
+            if (isset($_GET["channel"])) {
+              $chan = $_GET["channel"];
+              if (!ctype_digit($chan))
+                die("Invalid channel number: " . $chan);
+            }
+            else
+              $chan = "11";
+            if (isset($_GET["hours"])) {
+              $hours = $_GET["hours"];
+              if (!ctype_digit($hours))
+                die("Invalid hours: " . $hours);
+            }
+            else
+              $hours = "24";
+            if (isset($_GET["interval"])) {
+              $interval = $_GET["interval"];
+              if (!ctype_digit($interval))
+                die("Invalid interval: " . $interval);
+            }
+            else
+              $interval = "60";
+
+            // get channel name and multiplier
+            $sql = "SELECT * FROM channel WHERE channum = " . $chan;
+            $result = $conn->query($sql);
+            if ($result->num_rows <= 0)
+              die("Invalid channel: " . $chan);
+            $row = $result->fetch_assoc();
+            $name = $row["name"];
+            $mult = $row["mult"];
+
+            $sql = "SELECT FROM_UNIXTIME(UNIX_TIMESTAMP(stamp) DIV " . $interval . " * " . $interval . ") AS Time, " .
+                       "ROUND(AVG(watts)) * " . $mult . " AS Power " .
+                   "FROM used " .
+                   "WHERE channum = " . $chan . " AND stamp > DATE_ADD(CURRENT_TIMESTAMP, INTERVAL -" . $hours . " HOUR) " .
+                   "GROUP BY UNIX_TIMESTAMP(stamp) DIV " . $interval;
+            $result = $conn->query($sql);
+            if ($result->num_rows <= 0)
+              die("No results for channel " . $chan . ", interval " . $interval);
+
+            while($row = mysqli_fetch_array($result)){
+              $time = $row['Time'];
+              $power = $row['Power'];
+              echo "[new Date('".$row['Time']."Z'),".$row['Power']."],";
+            }
+          ?>
+        ]);
+
+        var options = {
+          title: "<?php echo $name ?>"
+        };
+        var chart = new google.visualization.ColumnChart(document.getElementById("chart_div"));
+        chart.draw(data, options);
+      }
+    </script>
   </head>
   <body>
 
-    <?php
-      include 'nav.php';
-    ?>
+    <div class="content">
+      <div class="side">
 
-    <h1>
-      Greener Circuits
-    </h1>
-    <br/>
+        <?php
+          $sql = "SELECT * FROM channel WHERE inuse=1 ORDER BY name";
+          $result = $conn->query($sql);
 
-    <p>
-      Welcome to Greener Circuits.
-    </p>
-    <p>
-      My house has two 24-circuit Powerhouse Dynamics eMonitor power line monitors.
-    </p>
-    <p style="text-align: center">
-      (eMonitor photo here)
-    </p>
-    <p>
-      Each monitor contains a web server that will show the current power usage of one circuit. Powerhouse Dynamics
-      offers an annual subscription to provide history, and lets me analyze that history in many ways.
-      I thought it might be fun to write software to periodically retrieve data from these monitors, store it
-      in a database, and analyze it in ways that Power Dynamics does not offer. For example, I wanted it to
-      notify me if the ceiling fan in our unused bedroom was left on when nobody was in the room. This project
-      is the result.
-      <br/>
-    </p>
-    <p>
-      To implement this project I had to brush up on PHP, CSS, HTML, MySQL, Python, Linux system administration,
-      and third party libraries such as Beautiful Soup. All of the HTML, CSS, and PHP is hand-crafted.
-    </p>
-    <p style="text-align: center">
-      (Raspberry Pi Zero photo here)
-    </p>
-    <p>
-      This whole project runs on a Raspberry Pi Zero with an 8 GB Micro-SD card.
-      <br/>
-    </p>
-    <p>
-      Choose Usage from the menu bar above to see how usage has varied for each circuit over the past 24 hours.
-    </p>
+          if ($result->num_rows > 0) {
+            echo "<ul>";
+            while($row = $result->fetch_assoc()) {
+              echo "<li>" . "<a href='index.php?" .
+                "channel=" . $row["channum"] .
+                "&interval=" . $interval .
+                "&hours=" . $hours .
+                "'>" . $row["name"] . "</a></li>";
+            }
+            echo "</ul>";
+          } else {
+            echo "No channels in database";
+          }
+        ?>
+      </div>
 
+      <div id="chart_div" style="height:500px"></div>
+
+    </div>
   </body>
 </html>
 
