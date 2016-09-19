868aeed7cde320efda28da804278b3690249e885
Michael Rodby <github@rodby.org>
2016-09-19 06:58:49 +0000

    Move IP addresses to a text file: eMonitors Add notifications via prowl when too many eMonitor web page reads fail in a row Remove commented-out code to get name from channels Add several comments to show program flow Implement Python script to combine rows in "used" table to 1-minute intervals and delete rows older than history_days days

---
+++ db_daemon.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+import MySQLdb
+import requests
+import cgi
+import datetime
+import time
+import sys
+import os
+
+# function to send a notification via prowl.com
+# - call with event = IP address, desc = what happened
+def prowl(event, desc):
+  app = cgi.escape('Greener Circuits')
+  url = 'http://api.prowlapp.com/publicapi/add?' + \
+    'apikey=' + prowl_key + \
+    '&application=' + cgi.escape("Greener Circuits") + \
+    '&event=' + cgi.escape(event) + \
+    '&description=' + cgi.escape(desc)
+  requests.get(url)
+
+print '***** Starting Greener Circuits Database Daemon *****'
+sys.stdout.flush()
+
+# if prowl_key is set, copy to global; if not, complain and exit
+if not 'prowl_key' in os.environ:
+  print 'prowl_key not set in environment'
+  quit()
+prowl_key = os.environ['prowl_key']
+if prowl_key[-1] == ':':      # TODO: when started from /etc/init.d a colon is appended - find out why
+  prowl_key = prowl_key[:-1]
+
+# connect to database
+db = MySQLdb.connect(host="localhost",
+                     user="eMonitor",
+#                     passwd="xxxxxxxx", - will be filled in from .my.cnf
+                     db="eMonitor",
+                     read_default_file="/home/mrodby/.my.cnf")
+
+# start main loop
+while True:
+
+  # only do this loop once each minute
+  time.sleep(60)
+
+  now = datetime.datetime.now()
+
+  # get database cursor
+  cur = db.cursor()
+
+  # get the timestamp of the most recent consolidation
+  # if none, use oldest timestamp in used table
+  # if none, use now
+  consolidate_stamp = None
+  cur.execute("SELECT consolidate_stamp FROM settings");
+  row = cur.fetchone()
+  if row == None:
+    cur.execute("INSERT INTO settings VALUES(NULL)")
+  else:
+    consolidate_stamp = row[0]
+  if consolidate_stamp == None:
+    cur.execute("SELECT MIN(stamp) FROM used")
+    row = cur.fetchone()
+    if row != None:
+      consolidate_stamp = row[0]
+  if consolidate_stamp == None:
+    consolidate_stamp = now
+
+  # calculate start and end of consolidation period
+  # - move back to start of hour
+  start_stamp = consolidate_stamp.replace(minute=0, second=0, microsecond=0)
+  # - set end_stamp to one our later
+  end_stamp = start_stamp + datetime.timedelta(hours=1)
+  # - if end_stamp is not earlier than 1 minute ago, don't do anything this time around
+  if end_stamp >= now - datetime.timedelta(minutes=1):
+    continue
+
+  print "Consolidating from " + start_stamp.isoformat()[:19] + " to " + end_stamp.isoformat()[:19]
+  sys.stdout.flush()
+
+  # start a transaction to ensure nobody else sees inconsistent data from this consolidation
+  cur.execute("START TRANSACTION")
+
+  # consolidate rows from this time period
+  # - clear scratchpad table
+  cur.execute("DELETE FROM scratchpad")
+  # - consolidate into scratchpad table
+  sql = "INSERT INTO scratchpad " +\
+          "SELECT " +\
+             "channum, " +\
+             "AVG(watts) AS watts, " +\
+             "FROM_UNIXTIME(UNIX_TIMESTAMP(stamp) DIV 60 * 60 ) AS stamp " +\
+          "FROM used " +\
+          "WHERE stamp >= '" + start_stamp.isoformat() + "' AND stamp < '" + end_stamp.isoformat() + "' " +\
+          "GROUP BY channum, UNIX_TIMESTAMP(stamp) DIV 60"
+  cur.execute(sql)
+  # - delete original rows
+  sql = "DELETE FROM used "+\
+          "WHERE stamp >= '" + start_stamp.isoformat() + "' AND stamp < '" + end_stamp.isoformat() + "' "
+  cur.execute(sql)
+  # - copy from scratchpad table to original table
+  sql = "INSERT INTO used SELECT * FROM scratchpad"
+  cur.execute(sql)
+
+  # update consolidate_stamp in settings
+  cur.execute("UPDATE settings SET consolidate_stamp='" + end_stamp.isoformat() + "'")
+
+  # all done - commit transaction
+  cur.execute("COMMIT");
+
+  # done with this pass - close and commit
+  cur.close()
+  db.commit() # TODO: is this necessary?
+
+  print "Done: " + now.isoformat()[:19]
+
+  # Once an hour delete rows older than number of days specified in settings table
+  cur.execute("SELECT history_days FROM settings");
+  row = cur.fetchone()
+  if row == None:
+    days = 0
+  else:
+    days = row[0]
+  if days == None:
+    days = 0
+  if days != 0:
+    sql = "DELETE FROM used WHERE stamp < DATE_ADD(now, INTERVAL -(" + str(days) + " DAY)"
+    print sql
+
+
--- get_usage.py
+++ get_usage.py
@@ -6,16 +6,47 @@ import cgi
 import datetime
 import time
 import sys
+import os
 
-ips = ['192.168.1.124', '192.168.1.120']
-inc = 10  # update interval in seconds (max 60)
+# globals
+fails = [0, 0]
+max_fails = 30
+inc = 10  # read web page from each eMonitor IP every inc seconds (max 60)
 
+# function to send a notification via prowl.com
+# - call with event = IP address, desc = what happened
+def prowl(event, desc):
+  app = cgi.escape('Greener Circuits')
+  url = 'http://api.prowlapp.com/publicapi/add?' + \
+    'apikey=' + prowl_key + \
+    '&application=' + cgi.escape("Greener Circuits") + \
+    '&event=' + cgi.escape(event) + \
+    '&description=' + cgi.escape(desc)
+  requests.get(url)
+
+print '***** Starting Greener Circuits *****'
+sys.stdout.flush()
+
+# if prowl_key is set, copy to global; if not, complain and exit
+if not 'prowl_key' in os.environ:
+  print 'prowl_key not set in environment'
+  quit()
+prowl_key = os.environ['prowl_key']
+if prowl_key[-1] == ':':      # TODO: when started from /etc/init.d a colon is appended - find out why
+  prowl_key = prowl_key[:-1]
+
+# get IP addresses or hostnames from eMonitors file
+with open('eMonitors') as f:
+  ips = f.read().splitlines()
+
+# connect to database
 db = MySQLdb.connect(host="localhost",
                      user="eMonitor",
-#                     passwd="xxxxxxxx",
+#                     passwd="xxxxxxxx", - will be filled in from .my.cnf
                      db="eMonitor",
                      read_default_file="/home/mrodby/.my.cnf")
 
+# start main loop
 while True:
   while True:
     # wait for next time increment
@@ -24,10 +55,11 @@ while True:
       break
     time.sleep(0.5)
 
+# get database cursor and start a transaction
   cur = db.cursor()
   cur.execute("START TRANSACTION")
-  # get web page(s) from eMonitor(s)
-  nonBreakSpace = u'\xa0'
+
+# get web page(s) from eMonitor(s) - log and notify via Prowl when failures occur
   for idx, ip in enumerate(ips):
     try:
       response = requests.get('http://' + ip, timeout=5)
@@ -36,7 +68,24 @@ while True:
         continue
     except (requests.exceptions.ConnectTimeout, requests.exceptions.ReadTimeout, requests.exceptions.ConnectionError):
       print "Error reading page from " + ip
+      fails[idx] += 1
+      if fails[idx] == max_fails:
+        msg = 'eMonitor DOWN: ' + str(max_fails) + ' read failures'
+        prowl (ip, msg)
       continue
+    if fails[idx] >= max_fails:
+      msg = 'eMonitor UP after ' + str(fails[idx]) + ' read failures'
+      prowl (ip, msg)
+    if fails[idx] > 0:
+      msg = ip + ': success after ' + str(fails[idx])
+      if fails[idx] > 1:
+        msg += ' consecutive'
+      msg += ' failure'
+      if fails[idx] > 1:
+        msg += 's'
+      print now.isoformat()[:19] + ' ' + msg
+      sys.stdout.flush()
+      fails[idx] = 0
 
     # pass page through Beautiful Soup HTML parser, insert each row into database
     # - for channel number, use 100 + index for second eMonitor unit
@@ -46,7 +95,6 @@ while True:
       cells = row.findAll("td")
       if len(cells) == 3:
         num = str(int(cells[0].find(text=True)) + 100 * idx)
-    #    name = cells[1].find(text=True).strip(nonBreakSpace)
         watts = cells[2].find(text=True)
         sql = "INSERT INTO used VALUES (" + num + ", " + watts + ", '" + now.isoformat() + "')"
         cur.execute(sql)
@@ -56,11 +104,10 @@ while True:
 
   # done with this pass - close and commit
   cur.close()
-  db.commit()
+  db.commit() # TODO: is this necessary?
 
   # print update time
   print now.isoformat()[:19]
   sys.stdout.flush()
 
-db.close()
 
